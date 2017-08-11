9292312c611f0b6262b70f6fe30a12048a3c2a66
gsgalarnyk <gsgalarnyk@gmail.com>
2017-08-10 19:03:39 -0500

    asfasdfd

--- assets/javascript/movie.js
+++ assets/javascript/movie.js
@@ -1,5 +1,33 @@
-/*Week 8 homework Team The Directors  
+/*Week 8 homework Team The Directors
 https://github.com/KC0NUG/TheDirectors.git
 Team Members:
 Chucks @KC0NUG
 */
+/*Week 8 homework Team The Directors
+https://github.com/KC0NUG/TheDirectors.git
+Team Members:
+Chucks @KC0NUG
+*/
+var NetImgArray = [];
+
+$.ajax({
+        url: "https://netflixroulette.net/api/api.php?title=How%20to%20train%20your%20dragon",
+        method: "GET"
+      })
+
+      // After the data from the AJAX request comes back
+      .done(function(response) {
+        console.log(response);
+        // Saving the image_original_url property
+        // var imageUrl = response.data.image_original_url;
+
+        // // Creating and storing an image tag
+        // var catImage = $("<img>");
+
+        // // Setting the catImage src attribute to imageUrl
+        // catImage.attr("src", imageUrl);
+        // catImage.attr("alt", "cat image");
+
+        // // Prepending the catImage to the images div
+        // $("#images").prepend(catImage);
+      });
